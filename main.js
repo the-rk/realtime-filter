6c3ee24c699e9f7126a5e5f8b0cf1ade992d3607
the-rk <95013057+the-rk@users.noreply.github.com>
2022-05-03 16:00:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(350,300);
+    canvas.center();
+}
+ function draw(){
+
+ }
+
+ function take_snapshot(){
+     save("realtime_filter.png");
+ }
